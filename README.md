ef288cb4e90eb96d990715edc2581c00135a6437
Freewind <nowindlee@gmail.com>
2020-09-07 14:01:27 +0800

    typescript-react-input-type-number-clear-value-demo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Input "type=number" Clear Invalid Value Demo
+====================================================
+
+对于`type=number`类型的input，在react中使用有一个问题：即当输入的内容不是一个合法的数字时，`onChange`传给react的实际上是一个空字符串。
+在这种情况下，如果我们手动将inputValue的值设为空，不会引起重新render，不会清空其值。
+
+其中有一个办法是使用`useRef`拿到input，调用其`inputRef.current.value = ''`来手动清空，在本demo中演示。
+
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,25 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 export default function Hello() {
+  const [inputValue, setInputValue] = useState<string | undefined>('');
+
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    console.log("### > useEffect", {inputValue});
+    if (inputValue === undefined) {
+      if (inputRef.current) {
+        console.log('### force clear')
+        inputRef.current.value = '';
+      }
+    }
+  }, [inputValue])
+
   return <div>
-    <h1>Hello React</h1>
+    <h1>Type an invalid number in the number input</h1>
+    <input type='number' value={inputValue} onChange={(e) => setInputValue(e.target.value)} ref={inputRef}/>
+    <button onClick={() => setInputValue(undefined)}>Clear</button>
+    <hr/>
+    Input Value: {inputValue}
   </div>
 };
